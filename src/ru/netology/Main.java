06ec5dee71ba952bfdd8a34c149cb8eaf984705e
Anton Komrachkov <a.komrachkov@gmail.com>
2022-04-19 02:42:59 +0300

    Tasks done

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -1,8 +1,22 @@
 package ru.netology;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (Integer number : intList) {
+            if (number > 0) {
+                if (number % 2 == 0) {
+                    result.add(number);
+                }
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        System.out.println(result);
     }
 }
